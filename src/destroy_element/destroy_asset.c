71069a956167e4f4e165003aaafec45bab37648b
pierre1754 <pierre1754@outlook.com>
2020-12-07 16:01:07 +0100

    init asset finished

--- src/destroy_element/destroy_asset.c
+++ src/destroy_element/destroy_asset.c
@@ -7,9 +7,31 @@
 
 #include "my_runner.h"
 
+static void destroy_sounds(void)
+{
+    engine_t *engine = get_engine();
+
+    sfSound_destroy(GET_JUMP(engine));
+    sfSoundBuffer_destroy(GET_BUF_JUMP(engine));
+    free(engine->asset->sounds);
+}
+
+static void destroy_textures(void)
+{
+    engine_t *engine = get_engine();
+
+    for (int i = 0; i < texSize; i++) {
+        sfTexture_destroy(GET_TEXTURE(engine, i));
+        free(engine->asset->textures[i]);
+    }
+}
+
 void destroy_asset(void)
 {
     engine_t *engine = get_engine();
 
+    sfMusic_destroy(GET_MUSIC(engine));
+    destroy_sounds();
+    destroy_textures();
     free(engine->asset);
 }
