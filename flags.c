1458712aa0eaa2014865aecd99e9215a7f3a67a2
pierre1754 <pierre1754@outlook.com>
2021-01-09 18:21:38 +0100

    game finished

--- flags.c
+++ flags.c
@@ -11,7 +11,10 @@ int flag(char **av)
 {
     if (av[1][0] != '-' || av[1][1] != 'h')
         return 84;
-    write(0, "USAGES\n   ./my_hunter $(MAP_PATH)\n\n", 35);
-    write(0, "DECRIPTION\n   A infinite run game.\n", 35);
+    write(0, "USAGES\n   ./my_runner asset/map/...\n\n", 37);
+    write(0, "DECRIPTION\n  A finite runner game\n", 34);
+    write(0, "  Press 'Play' to launch the game.\n", 35);
+    write(0, "  Press space bar to jump.\n", 27);
+    write(0, "  To leave the game, press escape key or the exit button at the of the game.\n", 77);
     return 0;
 }
